1d85b020242d6b3dc281e8dd98ee0fa931787cd2
bdimpe@gmail <imaginarydevelopment@gmail.com>
2023-06-09 17:36:28 -0400

    http success and displayed

--- src/Main.elm
+++ src/Main.elm
@@ -1,52 +1,68 @@
 module Main exposing (..)
 
-
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 import Http
 import Json.Decode as D
 
--- type Model
---   = Failure
---   | Loading
---   | Success String
-
--- type alias Model =
---   { name : String
---   , password : String
---   , passwordAgain : String
---   }
+type BookState
+  = NotStarted
+  | Started
+  | Finished (Result Http.Error String)
 
 type alias Model =
   { counter : Int
-    , gotBookResult : Maybe (Result Http.Error String)
+    , bookState: BookState
+    -- , gettingBook : Bool
+    -- , gotBookResult : Maybe (Result Http.Error String)
     , gotItems: Maybe (Result Http.Error (List String))
   }
 
 type Msg
-  = GotBook (Result Http.Error String)
+  = GetBook
+  | GotBook (Result Http.Error String)
   | GotItems (Result Http.Error (List String))
   | Increment
   | Decrement
 
 main =
-  -- let view2 msg =
-  --   {title = "hello", body = [ view msg ] }
-  -- in
-  -- Browser.sandbox { init = 0, update = update, view = view }
-    -- Browser.document {init = init, view = view2, update = update, subscriptions = ignore}
     Browser.element { init = init, view = view, update = update, subscriptions = subscriptions }
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( { counter = 0, gotBookResult = Nothing, gotItems = Nothing }
+  ( { counter = 0, bookState = NotStarted, gotItems = Nothing }
   , Http.get
       { url = "https://elm-lang.org/assets/public-opinion.txt"
       , expect = Http.expectString GotBook
       }
   )
 
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    GetBook ->
+      ({model | bookState = Started}, getBook)
+    Increment ->
+      ({ model | counter = model.counter + 1 }, Cmd.none)
+    Decrement ->
+      ({ model | counter = model.counter - 1}, Cmd.none)
+    GotBook x ->
+      ({model | bookState = Finished x}, Cmd.none)
+    GotItems _ -> (model, Cmd.none)
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ div [] [ text (getBookText model) ]
+    , button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model.counter) ]
+    , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick GetBook ] [ text "GetBook" ]
+    ]
+
+subscriptions : Model -> Sub Msg
+subscriptions _ = Sub.none
 
 getBook : Cmd Msg
 getBook =
@@ -63,33 +79,29 @@ fetchItems =
     , expect = Http.expectJson GotItems (D.list (D.field "name" D.string))
     }
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-  case msg of
-    Increment ->
-      ({ model | counter = model.counter + 1 }, Cmd.none)
-    Decrement ->
-      ({ model | counter = model.counter - 1}, Cmd.none)
-    GotBook (Result.Ok b) -> (model, Cmd.none)
-    GotBook (Result.Err e) ->(model, Cmd.none)
-    GotItems _ -> (model, Cmd.none)
+getBookText : Model -> String
+getBookText model =
+  case model.bookState of
+    NotStarted -> "Nothing"
+    Started -> "Fetching..."
+    -- Finished (Result.Ok t) -> String.join "," t
+    Finished (Result.Ok t) -> t
+    Finished (Result.Err e) -> errorToString e
 
-view : Model -> Html Msg
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model.counter) ]
-    , button [ onClick Increment ] [ text "+" ]
-    -- , button [ onClick ]
-    ]
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Sub.none
--- main =
---   Browser.element
---     { init = init
---     , update = update
---     , subscriptions = subscriptions
---     , view = view
---     }
+errorToString : Http.Error -> String
+errorToString error =
+    case error of
+        Http.BadUrl url ->
+            "The URL " ++ url ++ " was invalid"
+        Http.Timeout ->
+            "Unable to reach the server, try again"
+        Http.NetworkError ->
+            "Unable to reach the server, check your network connection"
+        Http.BadStatus 500 ->
+            "The server had a problem, try again later"
+        Http.BadStatus 400 ->
+            "Verify your information and try again"
+        Http.BadStatus _ ->
+            "Unknown error"
+        Http.BadBody errorMessage ->
+            errorMessage
