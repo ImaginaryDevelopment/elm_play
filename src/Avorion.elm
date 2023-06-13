3cc871467cc95a5acbdfc275fe9d772ff9a23adb
bdimpe@gmail <imaginarydevelopment@gmail.com>
2023-06-13 10:52:39 -0400

    whitespace only - code formatting

--- src/Avorion.elm
+++ src/Avorion.elm
@@ -1,84 +1,108 @@
-module Avorion exposing (Msg, Model, Economy, initCmd, update, view)
+module Avorion exposing (Economy, Model, Msg, initCmd, update, view)
 
 import Dict
-
-import Json.Decode as D
-import Json.Decode exposing (Decoder)
-import Json.Encode as Encode
-import Http
 import Html exposing (..)
-
+import Http
+import Json.Decode as D exposing (Decoder)
+import Json.Encode as Encode
 import Shared exposing (..)
 
-type alias GoodInfo = {
-  cost: Int
-  , soldBy: Maybe String
-  }
+
+type alias GoodInfo =
+    { cost : Int
+    , soldBy : Maybe String
+    }
+
 
 type Msg
-  = GotEconomy (HttpResult (Dict.Dict String GoodInfo))
-type alias Economy = Dict.Dict String GoodInfo
+    = GotEconomy (HttpResult (Dict.Dict String GoodInfo))
 
-type alias Model = HttpState Economy
+
+type alias Economy =
+    Dict.Dict String GoodInfo
+
+
+type alias Model =
+    HttpState Economy
+
 
 initCmd : Cmd Msg
 initCmd =
-  Http.get {
-    url = "../json/economy.json" -- reactor assumes /src root
-    , expect = Http.expectJson GotEconomy economyDecoder
-      -- , expect = Http.expectJson GotItems (D.list (D.field "name" D.string))
-  }
+    Http.get
+        { url = "../json/economy.json" -- reactor assumes /src root
+        , expect = Http.expectJson GotEconomy economyDecoder
 
-update: Msg -> Model -> (Model, Cmd Msg)
+        -- , expect = Http.expectJson GotItems (D.list (D.field "name" D.string))
+        }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    GotEconomy httpRes ->
-      let _ = (dumpEconomy httpRes) in
-      (Just httpRes, Cmd.none)
+    case msg of
+        GotEconomy httpRes ->
+            let
+                _ =
+                    dumpEconomy httpRes
+            in
+            ( Just httpRes, Cmd.none )
+
 
 view : Model -> Html.Html Msg
 view model =
-  case model of
-      Nothing ->
-        div [] [ text "No Economy loaded"]
-      Just (Err e) -> div [] [errorToString e |> text]
-      Just (Ok ec) ->
-        div [] [
-          pre [] [ -- it appears there's no reflection in elm?
-            Encode.encode 0 (encodeEconomy ec)
-            |> text
-          ]
-        ]
+    case model of
+        Nothing ->
+            div [] [ text "No Economy loaded" ]
 
-dumpEconomy : (HttpResult (Dict.Dict String GoodInfo)) -> ()
+        Just (Err e) ->
+            div [] [ errorToString e |> text ]
+
+        Just (Ok ec) ->
+            div []
+                [ pre []
+                    [ -- it appears there's no reflection in elm?
+                      Encode.encode 0 (encodeEconomy ec)
+                        |> text
+                    ]
+                ]
+
+
+dumpEconomy : HttpResult (Dict.Dict String GoodInfo) -> ()
 dumpEconomy httpRes =
-  case httpRes of
-      Result.Ok v ->
-        Debug.log "Got economy!"
-        ()
-      Result.Err e ->
-        Debug.log ( "Failed to get economy and/or deserialize:" ++ errorToString e)
-        ()
+    case httpRes of
+        Result.Ok v ->
+            Debug.log "Got economy!"
+                ()
+
+        Result.Err e ->
+            Debug.log ("Failed to get economy and/or deserialize:" ++ errorToString e)
+                ()
+
 
 encodeGoodInfo : GoodInfo -> Encode.Value
 encodeGoodInfo gi =
-  Encode.object
-    [ ("cost", Encode.int gi.cost)
-    , ("soldBy", gi.soldBy |> Maybe.withDefault "" |> Encode.string)
-    ]
+    Encode.object
+        [ ( "cost", Encode.int gi.cost )
+        , ( "soldBy", gi.soldBy |> Maybe.withDefault "" |> Encode.string )
+        ]
+
+
+
 -- https://package.elm-lang.org/packages/elm/json/latest/Json.Decode
 
 
 encodeEconomy : Economy -> Encode.Value
-encodeEconomy ec = -- https://package.elm-lang.org/packages/elm/json/latest/Json.Encode
-  Encode.dict identity encodeGoodInfo ec
+encodeEconomy ec =
+    -- https://package.elm-lang.org/packages/elm/json/latest/Json.Encode
+    Encode.dict identity encodeGoodInfo ec
 
-decodeGoodInfo: Decoder GoodInfo
+
+decodeGoodInfo : Decoder GoodInfo
 decodeGoodInfo =
-  D.map2 GoodInfo
-    (D.field "Cost" D.int)
-    (D.field "SoldBy" (D.maybe D.string))
+    D.map2 GoodInfo
+        (D.field "Cost" D.int)
+        (D.field "SoldBy" (D.maybe D.string))
+
 
 economyDecoder : Decoder (Dict.Dict String GoodInfo)
 economyDecoder =
-  D.dict decodeGoodInfo
+    D.dict decodeGoodInfo
--- src/Main.elm
+++ src/Main.elm
@@ -1,144 +1,178 @@
 module Main exposing (..)
 
-import Dict
-
-import Browser
 -- import Html exposing (Html, button, div, text, ul, li, a)
+
+import Avorion
+import Book
+import Browser
+import Dict
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Http
-import Json.Decode as D
-import Json.Decode exposing (Decoder)
+import Json.Decode as D exposing (Decoder)
 import Json.Encode as Encode
-
 import Shared exposing (..)
 
-import Avorion
-import Book
 
 
 -- import Json.Encode
-
 -- chaining https://allo-media.net/en/tech/learning/elm/2018/02/05/chaining-http-requests-in-elm.html
 
+
 type AppSection
-  = CounterSect
-  | BookSect
-  | AvorSect
-
-
+    = CounterSect
+    | BookSect
+    | AvorSect
 
 
 type alias Model =
-  { counter : Int
-    , display: AppSection
+    { counter : Int
+    , display : AppSection
     , bookModel : Book.Model
-    , avorionModel: Avorion.Model
-  }
+    , avorionModel : Avorion.Model
+    }
+
+
 type MenuMsg
-  = GoCounter
-  | GoBook
-  | GoAvorion
+    = GoCounter
+    | GoBook
+    | GoAvorion
+
+
 type CounterMsg
-  = Increment
-  | Decrement
+    = Increment
+    | Decrement
+
 
 type Msg
-  = MenuMsg MenuMsg
-  | BookMsg Book.Msg
-  | CounterMsg CounterMsg
-  | AvorionMsg Avorion.Msg
-  | Reset
+    = MenuMsg MenuMsg
+    | BookMsg Book.Msg
+    | CounterMsg CounterMsg
+    | AvorionMsg Avorion.Msg
+    | Reset
 
-initialState = { counter = 0, bookModel = Book.init, display = CounterSect, avorionModel = Nothing}
+
+initialState =
+    { counter = 0, bookModel = Book.init, display = CounterSect, avorionModel = Nothing }
+
 
 main =
     Browser.element { init = init, view = view, update = update, subscriptions = subscriptions }
 
-init : () -> (Model, Cmd Msg)
-init _ = ( initialState,Cmd.map AvorionMsg Avorion.initCmd )
 
-updateMenu : MenuMsg -> Model -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialState, Cmd.map AvorionMsg Avorion.initCmd )
+
+
+updateMenu : MenuMsg -> Model -> ( Model, Cmd Msg )
 updateMenu msg model =
-  case msg of
-      GoCounter -> ({model | display = CounterSect}, Cmd.none)
-      GoBook -> ({model | display = BookSect}, Cmd.none)
-      GoAvorion -> ({model | display = AvorSect}, Cmd.none)
+    case msg of
+        GoCounter ->
+            ( { model | display = CounterSect }, Cmd.none )
 
-updateCounter : CounterMsg -> Model -> (Model, Cmd Msg)
+        GoBook ->
+            ( { model | display = BookSect }, Cmd.none )
+
+        GoAvorion ->
+            ( { model | display = AvorSect }, Cmd.none )
+
+
+updateCounter : CounterMsg -> Model -> ( Model, Cmd Msg )
 updateCounter msg model =
-  case msg of
-    Increment ->
-      ({ model | counter = model.counter + 1 }, Cmd.none)
-    Decrement ->
-      ({ model | counter = model.counter - 1}, Cmd.none)
+    case msg of
+        Increment ->
+            ( { model | counter = model.counter + 1 }, Cmd.none )
 
-update : Msg -> Model -> (Model, Cmd Msg)
+        Decrement ->
+            ( { model | counter = model.counter - 1 }, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    MenuMsg mMsg ->
-      updateMenu mMsg model
-    CounterMsg cMsg ->
-      updateCounter cMsg model
-    BookMsg bMsg ->
-      let (bModel, bCmd) = Book.update bMsg model.bookModel
-      in
-      ({model | bookModel = bModel}, Cmd.map BookMsg bCmd)
-    AvorionMsg aMsg ->
-      let (aModel, aCmd) = Avorion.update aMsg model.avorionModel
-      in
-      ({model | avorionModel = aModel}, Cmd.map AvorionMsg aCmd )
-    Reset ->
-      (initialState, Cmd.map AvorionMsg Avorion.initCmd)
+    case msg of
+        MenuMsg mMsg ->
+            updateMenu mMsg model
+
+        CounterMsg cMsg ->
+            updateCounter cMsg model
+
+        BookMsg bMsg ->
+            let
+                ( bModel, bCmd ) =
+                    Book.update bMsg model.bookModel
+            in
+            ( { model | bookModel = bModel }, Cmd.map BookMsg bCmd )
+
+        AvorionMsg aMsg ->
+            let
+                ( aModel, aCmd ) =
+                    Avorion.update aMsg model.avorionModel
+            in
+            ( { model | avorionModel = aModel }, Cmd.map AvorionMsg aCmd )
+
+        Reset ->
+            ( initialState, Cmd.map AvorionMsg Avorion.initCmd )
+
 
 createMenu isSelected msg title =
-  if isSelected then
-    a [href "#"] [text title]
-  else
-    a [href "#", onClick (MenuMsg msg)][text title]
+    if isSelected then
+        a [ href "#" ] [ text title ]
+
+    else
+        a [ href "#", onClick (MenuMsg msg) ] [ text title ]
+
 
 viewMenu : Model -> Html Msg
 viewMenu model =
-  div [][
-    ul [] [
-      -- li [] [ a [href "#", onClick (MenuMsg GoCounter)][text "Counter"]]
-      li [] [ createMenu (model.display == CounterSect) GoCounter "Counter" ]
-      , li [] [ createMenu (model.display == BookSect) GoBook "Book"]
-      , li [] [ createMenu (model.display == AvorSect) GoAvorion "Avorion"]
-    ]
-  ]
+    div []
+        [ ul []
+            [ -- li [] [ a [href "#", onClick (MenuMsg GoCounter)][text "Counter"]]
+              li [] [ createMenu (model.display == CounterSect) GoCounter "Counter" ]
+            , li [] [ createMenu (model.display == BookSect) GoBook "Book" ]
+            , li [] [ createMenu (model.display == AvorSect) GoAvorion "Avorion" ]
+            ]
+        ]
+
 
 viewCounter : Model -> Html Msg
 viewCounter model =
-  div [] [
-      button [ onClick (CounterMsg Decrement) ] [ text "-" ]
-      , div [] [ text (String.fromInt model.counter) ]
-      , button [ onClick (CounterMsg Increment) ] [ text "+" ]
-  ]
+    div []
+        [ button [ onClick (CounterMsg Decrement) ] [ text "-" ]
+        , div [] [ text (String.fromInt model.counter) ]
+        , button [ onClick (CounterMsg Increment) ] [ text "+" ]
+        ]
+
 
 view : Model -> Html Msg
 view model =
-  let
-    subv =
-      (case model.display of
-        BookSect ->
-          let v = Book.view model.bookModel
-          in
-            Html.map BookMsg v
-        CounterSect -> viewCounter model
-        AvorSect ->
-          let v = Avorion.view model.avorionModel
-          in
-            Html.map AvorionMsg v
-      )
-  in
-  div []
-    [ viewMenu model
-      , subv
-    ]
+    let
+        subv =
+            case model.display of
+                BookSect ->
+                    let
+                        v =
+                            Book.view model.bookModel
+                    in
+                    Html.map BookMsg v
+
+                CounterSect ->
+                    viewCounter model
+
+                AvorSect ->
+                    let
+                        v =
+                            Avorion.view model.avorionModel
+                    in
+                    Html.map AvorionMsg v
+    in
+    div []
+        [ viewMenu model
+        , subv
+        ]
+
 
 subscriptions : Model -> Sub Msg
-subscriptions _ = Sub.none
-
-
+subscriptions _ =
+    Sub.none
